3227e4388284619101d9f1cf6fbf993c6f507ece
revolt313 <hrouhi.kh@gmail.com>
2013-02-04 20:29:32 +0330

    update pyheap

--- pyheap.py
+++ pyheap.py
@@ -3,7 +3,7 @@
 # the simple heap module for create heap of chailds and parents.
 #****************************************************************
 #                publiced with BSDlisance
-#						  2013
+#			  2013
 # author: revolt313(Hamid Rouhi)
 #################################################################
 __version__= '1.0.0'
@@ -12,15 +12,15 @@ def heap_gen(Data):
 	global tree,parent
 	tree=list(Data)
 	while len(tree)>1:
-		LCh=min(tree)
-		index=tree.index(LCh)
-		LChL=list(tree[index])
+		LeftChild=min(tree)
+		index=tree.index(LeftChild)
+		LeftChildIndex=list(tree[index])
 		del tree[index]
-		RCh=min(tree)
-		index=tree.index(RCh)
-		RChL=list(RCh)
+		RightChild=min(tree)
+		index=tree.index(RightChild)
+		RightChildIndex=list(RightChild)
 		del tree[index]
-		parent=(LChL[0]+RChL[0],LCh,RCh)
+		parent=(LeftChildIndex[0]+RightChildIndex[0],LeftChild,RightChild)
 		tree.append(parent)
 	return tree[0]
 
