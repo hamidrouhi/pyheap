a6f684a797ba253dc7aaba7cad35f430ccbb8e33
revolt313 <hrouhi.kh@gmail.com>
2013-03-18 07:51:23 +0330

    Made PEP8 Compaliant

--- pyheap.py
+++ pyheap.py
@@ -1,26 +1,26 @@
-#!/usr/bin/python
-#****************************************************************
-# the simple heap module for create heap of chailds and parents.
-#****************************************************************
-#                publiced with BSDlisance
-#			  2013
-# author: revolt313(Hamid Rouhi)
-#################################################################
-__version__= '1.0.0'
+'''
+	this module genarate tree of chars with it frequency,
+	this appear for some alghorithm like Huffman alghorithm
+
+	you can use this like other modules,
+
+	WARNING: this module is't work if you give bad data,
+	for more info see this http://github.com/revolt313/pyheap
+'''
+__version__ = '1.0.0'
+
 
 def heap_gen(Data):
-	global tree,parent
-	tree=list(Data)
-	while len(tree)>1:
-		LeftChild=min(tree)
-		index=tree.index(LeftChild)
-		LeftChildIndex=list(tree[index])
+	tree = list(Data)
+	while len(tree) > 1:
+		LeftChild = min(tree)
+		index = tree.index(LeftChild)
+		LeftChildIndex = list(tree[index])
 		del tree[index]
-		RightChild=min(tree)
-		index=tree.index(RightChild)
-		RightChildIndex=list(RightChild)
+		RightChild = min(tree)
+		index = tree.index(RightChild)
+		RightChildIndex = list(RightChild)
 		del tree[index]
-		parent=(LeftChildIndex[0]+RightChildIndex[0],LeftChild,RightChild)
+		parent = (LeftChildIndex[0] + RightChildIndex[0], LeftChild, RightChild)
 		tree.append(parent)
 	return tree[0]
-
